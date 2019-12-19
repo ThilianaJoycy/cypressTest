66c311c6894cc6b37549866cf2e0a76ec9fefd96
yannick.mengue <yannick.mengue>
2019-12-19 10:32:25 +0100

    Initial version of gateway generated by JHipster-6.5.0

---
+++ src/main/java/com/sprint/minfi/gateway/config/GatewayConfiguration.java
@@ -0,0 +1,32 @@
+package com.sprint.minfi.gateway.config;
+
+import io.github.jhipster.config.JHipsterProperties;
+
+import com.sprint.minfi.gateway.gateway.accesscontrol.AccessControlFilter;
+import com.sprint.minfi.gateway.gateway.responserewriting.SwaggerBasePathRewritingFilter;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfiguration {
+
+    @Configuration
+    public static class SwaggerBasePathRewritingConfiguration {
+
+        @Bean
+        public SwaggerBasePathRewritingFilter swaggerBasePathRewritingFilter(){
+            return new SwaggerBasePathRewritingFilter();
+        }
+    }
+
+    @Configuration
+    public static class AccessControlFilterConfiguration {
+
+        @Bean
+        public AccessControlFilter accessControlFilter(RouteLocator routeLocator, JHipsterProperties jHipsterProperties){
+            return new AccessControlFilter(routeLocator, jHipsterProperties);
+        }
+    }
+
+}
---
+++ src/main/java/com/sprint/minfi/gateway/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.sprint.minfi.gateway.web.rest;
